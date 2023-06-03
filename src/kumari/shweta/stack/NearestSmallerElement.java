77141077ecd6b89a553d7bc736d89c884ea02179
shwetakiit <shweta0729e2open@gmail.com>
2023-06-03 13:13:38 +0530

    Nearest smaller element in right hand side

--- src/kumari/shweta/stack/NearestSmallerElement.java
+++ src/kumari/shweta/stack/NearestSmallerElement.java
@@ -6,11 +6,15 @@ package kumari.shweta.stack;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Stack;
 /*For given array find nearest smaller  element in left hand side . Eg arr[]={4, 6, 10, 11, 7, 6, 3, 5}
  *If  there is no nearest smaller element in left hand side use default value -1
  *Output --> [-1, 4, 6, 10, 6, 4, -1, 3]
+ *
+ *Find nearest smaller element in right hand side [7,9,3,5,8,11,6]
+ *Output -->[3,3,-1,-1,6,6,-1]
  */
 
 public class NearestSmallerElement {
@@ -60,11 +64,38 @@ public class NearestSmallerElement {
 		return result;
 	}
 
+	public static List<Integer> NextSmaller(List<Integer> input) {
+		Stack<Integer> stack = new Stack<>();
+		List<Integer> result = new ArrayList<>(input.size());
+		//Iterate from right to left for smaller element in right hand side 
+
+		for (int i = input.size()-1; i>=0;i --) {
+			// Pop all greater than equal element from stack
+			while (!stack.isEmpty() && (stack.peek() >= input.get(i))) {
+				stack.pop();
+			}
+			// Update result
+			if (stack.isEmpty() == true) {
+				result.add(-1);
+			} else {
+				result.add(stack.peek());
+			}
+			//Push element in stack
+			stack.push(input.get(i));
+		}
+		//Reverse result to maintain order corresponding to input list
+		Collections.reverse(result);
+		return result;
+	}
+
+	
 	public static void main(String[] args) {
 		List<Integer> input = Arrays.asList(4, 6, 10, 11, 7, 6, 3, 5);
 		List<Integer> result = prevSmaller(input);
-		System.out.println("Corresponding nearest element for each element " + result);
+		System.out.println("Smaller left side  nearest element for each element " + result);
 		List<Integer> bfResult = prevSmallerBF(input);
-		System.out.println("Corresponding nearest element for each element  "+bfResult);
+		System.out.println("Smaller left side  element for each element  "+bfResult);
+		List<Integer> rSmaller = NextSmaller(input);
+		System.out.println("Smaller right hadn side nearest element"+rSmaller);
 	}
 }
