4af1cd839da06f343e25f9e36de13ab2acf5c5cc
shwetakiit <shweta0729e2open@gmail.com>
2023-07-26 10:18:10 +0530

    Insert node in Binary search tree

--- src/kumari/shweta/tree/BinarySearchTree.java
+++ src/kumari/shweta/tree/BinarySearchTree.java
@@ -38,21 +38,67 @@ public class BinarySearchTree {
 		return 0;
 	}
 
+	/**
+	 * Insert node in binary search tree
+	 * 
+	 * @param args
+	 */
+	public static void insertNodeInBinarySearchTree(TreeNode node, int element) {
+		TreeNode temp = node;
+		TreeNode parentNode = null; // Parent node for root node is NULL;
+		while (temp != null) {
+			parentNode = temp;
+			if (temp.data == element) { // If Node is already exist in given BST
+				return;
+			} else if (temp.data < element) {
+				temp = temp.right;
+			} else {
+				temp = temp.left;
+			}
+		}
+		if (parentNode == null) {
+			node = new TreeNode(element);
+		} else if (parentNode.data < element) {
+			parentNode.right = new TreeNode(element);
+		} else {
+			parentNode.left = new TreeNode(element);
+		}
+
+		// Traverse BST after inserting node in Binary tree
+		inOrderTraversal(node);
+
+	}
+
+	public static void inOrderTraversal(TreeNode root) {
+		if (root == null) {
+			return;
+		}
+		inOrderTraversal(root.left);
+		System.out.print("\t" + root.data);
+		inOrderTraversal(root.right);
+	}
 	public static void main(String[] args) {
 
-		TreeNode root = new TreeNode(1);
-		root.left = new TreeNode(2);
-		root.right = new TreeNode(3);
-		root.left.left = new TreeNode(4);
-		root.left.right = new TreeNode(5);
+		// Create binary search tree
+		TreeNode root = new TreeNode(8);
+		root.left = new TreeNode(4);
+		root.left.left = new TreeNode(1);
+		root.left.right = new TreeNode(7);
+		root.right = new TreeNode(13);
+		root.right.left = new TreeNode(10);
+		root.right.right = new TreeNode(15);
+		root.right.right.right = new TreeNode(17);
 
-		int found = findNodeInBinaryTree(root, 10);
+		int found = findNodeInBinaryTree(root, 13);
 		if (found == 1) {
 			System.out.println("Given element is found in BST");
 
 		} else {
 			System.out.println("Given element is not found in BST");
 		}
+
+		// Insert node in Binary search tree
+		insertNodeInBinarySearchTree(root, 14);
 	}
 
 }
