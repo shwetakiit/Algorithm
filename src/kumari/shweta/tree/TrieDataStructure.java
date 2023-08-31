c5bbfd7400f57627f3839c77681c222325f8e0d9
shwetakiit <shweta0729e2open@gmail.com>
2023-08-31 10:52:53 +0530

    delete word from dictionary using trie datastructure

--- src/kumari/shweta/tree/TrieDataStructure.java
+++ src/kumari/shweta/tree/TrieDataStructure.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+
 /**
  * Given an array of words A (dictionary) and another array B (which contain
  * some words). You have to return the binary array (of length |B|) as the
@@ -93,7 +94,76 @@ public class TrieDataStructure {
 		return current.eow; // If last character of word is eow i.e. true return true .If that is not eow It
 							// means this word is prefix for some other word available in dictionary
 	}
+	
+	/**
+	 * Delete word from dictionary  TC -> O(length of word) SC -> O(1)
+	 * @param dictionary
+	 * @param word
+	 * @return
+	 */
 
+	protected boolean deleteWordFromDisctionary(List<String> dictionary, String word) {
+
+		Trie root = new Trie('1');
+
+		for (int i = 0; i < dictionary.size(); i++) {
+			insert(root, dictionary.get(i));
+		}
+
+		// Before deleting word check word is available or not in dictionary
+		boolean isWordExist = search(root, word);
+		if (!isWordExist) {
+			System.out.println("Word doesn't exist in dictionary");
+			return false;
+		}
+
+		Trie resultTrie = deleteWord(root, word);
+
+		/*
+		 * After deleting word from trie data structure (dictionary) search the word in
+		 * trie data structure for printing result to check word deleted or not
+		 */
+
+		boolean isWordDeleted = search(resultTrie, word) == false ? true : false;
+
+		return isWordDeleted;
+	}
+
+	protected Trie deleteWord(Trie root, String word) {
+
+		Trie current = root;
+		Trie temp = null;
+		char nearestChar = '_';
+		for (int i = 0; i < word.length(); i++) {
+			int count = 0;
+			for (int j = 0; j < 26; j++) {
+				if (current.childern[j] != null) {
+					count++;
+				}
+			}
+			if (count > 1 || current.eow == true) {
+				temp = current;
+				nearestChar = word.charAt(i);
+			}
+			int idx = word.charAt(i) - 'a'; // find index of target character
+			current = current.childern[idx];
+
+		}
+		current.eow = false;
+		int count = 0;
+		for (int j = 0; j < 26; j++) {
+			if (current.childern[j] != null) {
+				count++;
+			}
+		}
+		if (count > 0) {
+			return root;
+		} else {
+			temp.childern[nearestChar - 'a'] = null;
+		}
+		return root;
+
+	}
 	public static void main(String[] args) {
 
 		TrieDataStructure obj = new TrieDataStructure();
@@ -102,6 +172,10 @@ public class TrieDataStructure {
 		List<String> wordList = Arrays.asList("car", "tried", "try", "trie");
 		List<Boolean> result = obj.searchWordInDictionary(disctionary, wordList);
 		System.out.println("Find result of each word is found :true and  not found :false -->" + result);
+       
+		// Delete word from Dictionary list
+		boolean isWordDeleted = obj.deleteWordFromDisctionary(disctionary, "try");
+		System.out.println("Word deleted?  " + isWordDeleted);
 
 	}
 
