99d5da341208db54cadfeea0e53ab5d799343ffc
shwetakiit <shweta0729e2open@gmail.com>
2023-09-25 20:32:15 +0530

    Find nth largest element

--- src/kumari/shweta/tree/FindNthLargestUisngHeap.java
+++ src/kumari/shweta/tree/FindNthLargestUisngHeap.java
@@ -4,6 +4,7 @@
  */
 package kumari.shweta.tree;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -35,6 +36,42 @@ public class FindNthLargestUisngHeap {
 		return minHeap.get(0);
 	}
 
+/**
+  * Problem Description Given an integer array B of size N.You need to find the Ath largest element in the subarray [1 to i], where i varies from 1 to N. In other words, find the Ath largest element in the sub-arrays [1 : 1], [1 : 2], [1 : 3], ...., [1 : N].
+  * NOTE: If any subarray [1 : i] has less than A elements, then the output should be -1 at the ith index.
+  * 
+  *Problem Constraints 1 <= N <= 100000 1 <= A <= N 1 <= B[i] <= INT_MAX
+  *Input Format -->The first argument is an integer A. The second argument is an integer array B of size N.
+  *  
+  * Output Format --> Return an integer array C, where C[i] (1 <= i <= N) will have the Ath largest element in the subarray [1 : i].
+  * 
+  * Input A = 4   B[1 2 3 4 5 6]   Output --> [-1, -1, -1, 1, 2, 3]
+**/
+	
+	private List<Integer > findNthLargestUsingMaxHeap(List<Integer> list, int k){
+		
+		HeapDataStructureHelper obj = new HeapDataStructureHelper();
+		List<Integer> minHeap = obj.buildHeap(list.subList(0, 3));
+		List<Integer> result = new ArrayList<>();
+        list.forEach(i->result.add(-1));
+        result.set(k-1, minHeap.get(0));
+		
+		for (int i = k; i < list.size(); i++) {
+			int root = minHeap.get(0);
+			if (root < list.get(i)) {
+				minHeap.set(0, list.get(i));
+				obj.heapify(minHeap, 0);
+				result.set(i,minHeap.get(0));
+			} else {
+				result.set(i,minHeap.get(0));
+			}
+		}
+		
+	return result; 
+		
+		
+		
+	}
 	public static void main(String[] args) {
 
 		FindNthLargestUisngHeap obj = new FindNthLargestUisngHeap();
@@ -42,5 +79,14 @@ public class FindNthLargestUisngHeap {
 		int k = 3;
 		int kthLargestNode = obj.findNthLargestNode(list, k);
 		System.out.println("Kth largest element is " + kthLargestNode);
+
+		List<Integer> list1 = Arrays.asList(1, 2, 3, 4, 5, 6);
+		List<Integer> list2 = Arrays.asList(15, 20, 99, 1);
+
+		List<Integer> output1 = obj.findNthLargestUsingMaxHeap(list2, 2);
+		System.out.println(output1);
+
+		List<Integer> output2 = obj.findNthLargestUsingMaxHeap(list1, 3);
+		System.out.println(output2);
+		}
 	}
-}
