6e3e8af6b8430c740f01d3b01dac045fd4e86e39
shwetakiit <shweta0729e2open@gmail.com>
2023-10-23 12:17:40 +0530

    Permutation with duplicate elements using backtracking

--- src/kumari/shweta/backtracking/Permutations.java
+++ src/kumari/shweta/backtracking/Permutations.java
@@ -5,9 +5,16 @@
 package kumari.shweta.backtracking;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+
+
+
 
 public class Permutations {
 	ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+    ArrayList<ArrayList<Character>> answer = new ArrayList<>();
 
 	public ArrayList<ArrayList<Integer>> findAllPermutations(ArrayList<Integer> inpuList) {
 
@@ -46,12 +53,71 @@ public class Permutations {
 			}
 		}
 	}
+	
+	/*
+	 * Find all permutation ,If Given list has duplicate elements TC O(N!), SC
+	 * ->O(N)
+	 */
+	public ArrayList<ArrayList<Character>> findPermutations(ArrayList<Character> inpuList) {
 
+		Map<Character, Integer> freqency = new HashMap<>();
+		ArrayList<Character> result = new ArrayList<>(3);
+
+		for (int i = 0; i < inpuList.size(); i++) {
+
+			if (freqency.containsKey(inpuList.get(i))) {
+				int freq = freqency.get(inpuList.get(i));
+				freq = freq + 1;
+				freqency.put(inpuList.get(i), freq);
+			} else {
+				freqency.put(inpuList.get(i), 1);
+			}
+		}
+
+		calPermutation(freqency, inpuList.size(), result, 0);
+		return answer;
+
+	}
+
+	/**
+	 * @param freqency
+	 * @param size
+	 * @param result
+	 * @param i
+	 */
+	private void calPermutation(Map<Character, Integer> freqency, int size, ArrayList<Character> result, int idx) {
+
+		if (idx == size) {
+			answer.add(new ArrayList<>(result));
+			return;
+		}
+
+		for (Character c : freqency.keySet()) {
+
+			int value = freqency.get(c);
+			if (value > 0) {
+				int freq = value;
+				freq = freq - 1;
+				freqency.put(c, freq);
+				result.add(idx, c);
+				calPermutation(freqency, size, result, idx + 1);
+				result.remove(result.size() - 1);
+				freqency.put(c, ++freq);
+			}
+
+		}
+	}
 	public static void main(String[] args) {
 		Permutations obj = new Permutations();
 		ArrayList<Integer> inputList = new ArrayList<>();
 		inputList.add(1);inputList.add(2);inputList.add(3);
 		ArrayList<ArrayList<Integer>> result = obj.findAllPermutations(inputList);
 		System.out.println("All permutation list" + result);
+
+		ArrayList<Character> charList = new ArrayList<>();
+		charList.add('a');charList.add('b');charList.add('a');
+		ArrayList<ArrayList<Character>> resList = obj.findPermutations(charList);
+		System.out.println("Find all permutations" + resList);
+		
 	}
 }
