f239792c847f40f37211b624d0d2a460ac2c6081
shwetakiit <shweta0729e2open@gmail.com>
2024-06-21 11:07:49 +0530

    Insert at given position in Doubly linked list

--- src/kumari/shweta/LinkedList/DoublyLinkedList.java
+++ src/kumari/shweta/LinkedList/DoublyLinkedList.java
@@ -1,9 +1,10 @@
 
 package kumari.shweta.LinkedList;
 
+
+
 /**
  * @author Shweta Kumari
- * @Email id shwetakumarikiit0729@gmail.com
  *
  */
 class DoublyNode {
@@ -25,14 +26,18 @@ public class DoublyLinkedList {
 	 * Case 1 :If Linked List is Empty 
 	 * Case 2: If Linked List is not Empty
 	 */
-	public void insertAtBeginning(int data) {
+	public DoublyNode insertAtBeginning(int data) {
 		DoublyNode node = new DoublyNode(data);
+		
+		if(head==null) {
+			head=node;
+			return head;
+		}
+		
 		node.next = head;
 		node.prev = null; // Just for code reading easy not required by default node prev value is null
-		if (head != null) {
-			head.prev = node;
-		}
 		head = node;
+		return head;
 	}
 
 	// Insert Node at End
@@ -40,7 +45,7 @@ public class DoublyLinkedList {
 	 * Case 1: If Linked List is Empty 
 	 * Case 2: If Linked List is not Empty
 	 */
-	public void insertNodeAtEnd(int data) {
+	public DoublyNode insertNodeAtEnd(int data) {
 		DoublyNode node = new DoublyNode(data);
 
 		if (head == null) {
@@ -48,6 +53,9 @@ public class DoublyLinkedList {
 			// node.next=null; //Not Required ,When We create oject of node next prev value
 			// is null
 			// node.prev=null;
+			
+			return head;
+			
 		} else {
 			DoublyNode temp = head;
 			while (temp.next != null) {
@@ -57,17 +65,21 @@ public class DoublyLinkedList {
 			temp.next = node;
 			node.next = null;
 			node.prev = temp;
+			
+			
 		}
+		return head;
 	}
 
 	// Insert Node after a item
 	/*
 	 * Three Cases need to handle 
-	 * 1.If Linked List is Empy 
+	 * 1.If Linked List is Empty 
+	 * 
 	 * 2. If we are adding in between linked list 
 	 * 3 If it is going to be last element of Linked List
 	 */
-	public void insertAfterANode(int data, int item) {
+	public DoublyNode insertAfterANode(int data, int item) {
 		boolean isFlag = false;
 		if (head == null) {
 			System.out.println("Empty Linked List");
@@ -79,9 +91,13 @@ public class DoublyLinkedList {
 				if (temp.data == item) {
 					node.next = temp.next;
 					node.prev = temp;
+					
+					if(temp.next!=null) { //If temp is last node then temp.next is null
 					temp.next.prev = node;
+                    }    
 					temp.next = node;
 					isFlag = true;
+					return head;
 				}
 				temp = temp.next;
 
@@ -90,6 +106,7 @@ public class DoublyLinkedList {
 				System.out.println("Item not found");
 			}
 		}
+		return head;
 
 	}
 
@@ -102,21 +119,28 @@ public class DoublyLinkedList {
 	 * adding in between node
 	 */
 
-	public void insertNodeBeforeItem(int data, int item) {
+	public DoublyNode insertNodeBeforeItem(int data, int item) {
 		boolean itemFound = false;
 		DoublyNode node = new DoublyNode(data);
-		DoublyNode temp = head;
+	
 
 		if (head == null) {
+			
 			System.out.println("Empty Linked List");
-		} else if (temp.data == item) {
-			node.next = head;
+			return head;
+			
+		} else if (head.data == item) { //If single node 
+			
+			DoublyNode temp = head;
+			node.next =temp;
 			node.prev = null;
-			head.prev = node;
+			temp.prev = node;
 			head = node;
 			itemFound = true;
-
+            return head;
+            
 		} else {
+			DoublyNode temp = head;
 			while (temp != null) {
 				if (temp.data == item) {
 
@@ -125,6 +149,7 @@ public class DoublyLinkedList {
 					temp.prev.next = node;
 					temp.prev = node;
 					itemFound = true;
+					return head;
 
 				}
 				temp = temp.next;
@@ -132,6 +157,50 @@ public class DoublyLinkedList {
 		}
 		if (!itemFound) {
 			System.out.println("Item not found ");
+		}
+		return head;
+	}
+
+	public DoublyNode insertNodeAtGivenPosition(int data, int position) {
+
+		DoublyNode node = new DoublyNode(data);
+		if (position <= 0) {
+			DoublyNode temp = head;
+			node.next = temp;
+			node.prev = null;
+
+			if (temp != null) { // If head is null
+				temp.prev = node;
+			}
+
+			head = node;
+			return head;
+		} else {
+			int index = 0;
+			DoublyNode temp = head;
+			while (index != position - 1 && temp.next != null) {
+				temp = temp.next;
+				index++;
+			}
+			if (position - 1 > index) { // If position crossed the list length of list. Add it at end
+
+				node.prev = temp;
+				node.next = null;
+				temp.next = node;
+
+			} else {
+
+				node.next = temp.next;
+				node.prev = temp;
+
+				if (temp.next != null) { // If it is last node then temp.next is null;
+					temp.next.prev = node;
+				}
+
+				temp.next = node;
+
+			}
+			return head;
 		}
 	}
 
@@ -171,6 +240,7 @@ public class DoublyLinkedList {
 		}
 	}
 
+	
 	public void deleteGivenItem(int item) {
 		boolean isDeleted = false;
 		DoublyNode temp = head;
@@ -248,6 +318,20 @@ public class DoublyLinkedList {
 
 	}
 
+	public void traversalNode(DoublyNode headNode) {
+
+		if (headNode == null) {
+			System.out.println("Empty Linked List");
+		}
+		DoublyNode temp = headNode;
+		while (temp != null) {
+
+			System.out.println(temp.data);
+			temp = temp.next;
+
+		}
+
+	}
 	public static void main(String[] args) {
 
 		DoublyLinkedList linkedList = new DoublyLinkedList();
@@ -255,25 +339,38 @@ public class DoublyLinkedList {
 			linkedList.insertAtBeginning(i);
 		}
 		linkedList.traversal();
-		/*System.out.println("Add node at end ....");
-		linkedList.insertNodeAtEnd(99);
-		linkedList.traversal();
+
+		System.out.println("Add node at end ....");
+		DoublyNode headNode = linkedList.insertNodeAtEnd(99);
+		linkedList.traversalNode(headNode);
+
 		System.out.println("Insert Node after given Item");
-		linkedList.insertAfterANode(100, 1);
-		linkedList.traversal();
+		DoublyNode headNode2 = linkedList.insertAfterANode(100, 99);
+		linkedList.traversalNode(headNode2);
+
 		System.out.println("Insert Node before given Item");
-		linkedList.insertNodeBeforeItem(1000, 4);
-		linkedList.traversal();
+		DoublyNode headNode3 = linkedList.insertNodeBeforeItem(1000, 99);
+		linkedList.traversalNode(headNode3);
+
+		System.out.println("Insert node at given position");
+		DoublyNode headNode4 = linkedList.insertNodeAtGivenPosition(999, 8);
+		linkedList.traversalNode(headNode4);
+
 		System.out.println("Delete first Node ");
 		linkedList.deleteFirstNode();
+		linkedList.traversal();
+
 		linkedList.insertAtBeginning(100);
 		linkedList.traversal();
+
 		System.out.println("Delete last node ");
 		linkedList.deleteNodeAtEnd();
 		linkedList.traversal();
+
 		System.out.println("Delete given item");
 		linkedList.deleteGivenItem(4);
-		linkedList.traversal();*/
+		linkedList.traversal();
+
 		System.out.println("Delete item at given position");
 		linkedList.deleteNoteAtGivenPosition(6);
 		linkedList.traversal();
