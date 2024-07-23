9818f082c1962133813e92fe8ea5f50e50075214
shwetakiit <shweta0729e2open@gmail.com>
2024-07-23 10:13:46 +0530

    Search element in array using binary search

---
+++ src/kumari/shweta/searching/BinarySearchInArray.java
@@ -0,0 +1,37 @@
+/**
+ * @author Shweta Kumari
+ *2024-07-23
+ */
+package kumari.shweta.searching;
+
+public class BinarySearchInArray {
+
+	// TC ->O(logN)
+	public static int findElementInSortedArray(int arr[], int k) {
+
+		int left = 0;
+		int right = arr.length - 1;
+		while (left <= right) {
+
+			int mid = (left + right) / 2; // For huge data use mid= left+(right-left)/2
+			if (arr[mid] == k) {
+				return mid;
+			} else if (arr[mid] < k) {
+				left = mid + 1;
+
+			} else {
+				right = mid - 1;
+			}
+		}
+
+		return -1;
+
+	}
+
+	public static void main(String[] args) {
+
+		int arr[] = { 3, 6, 9, 12, 14, 19, 20, 23, 25, 27 };
+		int k = findElementInSortedArray(arr, 20);
+		System.out.println("Element foundt at " + k);
+	}
+}
