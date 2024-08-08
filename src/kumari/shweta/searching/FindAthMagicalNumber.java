a5b4f2a2aba7f56cf7203acb9b03b73601c45b2f
shwetakiit <shweta0729e2open@gmail.com>
2024-08-08 10:02:33 +0530

    Find Ath Magical no using BS correctio

--- src/kumari/shweta/searching/FindAthMagicalNumber.java
+++ src/kumari/shweta/searching/FindAthMagicalNumber.java
@@ -20,59 +20,65 @@ Output Format
 Return the Ath smallest magical number. Since the answer may be very large, return modulo 109 + 7.
 */
 public class FindAthMagicalNumber {
-	
-	int findAthMagicalNumber(int A,int B,int C) {
-		
- 		int left=1; //We can take left=Min(B,C) 
-		int right=A*(Math.min(B,C));
-		int ans=0;
-		while(left<=right) {
-			
-			int mid=(left+right)/2;
-			int count=countOfMagincalNumber(B,C,mid);
-			if(count==A) {
-				ans=mid; //If we found the Magical count we should  go left side to find first occurance  of magical count.
-				right=mid-1;  //Move left to find better answer 
-			} else if(count>A) {
-				right=mid-1;
+
+	int findAthMagicalNumber(int A, int B, int C) {
+
+		long MOD = 1000000007;
+		long lcm = findLCM(B, C);
+		long left = 1; // We can take left=Min(B,C)
+		long minNum = (Math.min(B, C));
+		long right = A * minNum; // Here was issue due to mathematical operation was not correct
+		long ans = right;
+		while (left <= right) {
+			long mid = (left + right) / 2;
+			long count = countOfMagincalNumber(B, C, mid, lcm);
+
+			if (count == A) {
+				ans = mid;
+				right = mid - 1;
+			} else if (count > A) {
+				right = mid - 1;
 			} else {
-				left=mid+1;
+				left = mid + 1;
 			}
 		}
-		return ans;
+		return (int) (ans % MOD);
 	}
-	
 
-	private int countOfMagincalNumber(int b, int c, int mid) {
-		int lcm= findLCM(b,c);
-		int count= (mid/b)+(mid/c)-(mid/lcm);
+	private long countOfMagincalNumber(long b, long c, long mid, long lcm) {
+
+		long count = (mid / b) + (mid / c) - (mid / lcm);
 		return count;
 	}
 
-	private int findLCM(int b, int c) {
-		
-		int dvd=c;
-		int div=b;
-		while(dvd%div!=0) {
-		  int	rem=dvd%div;
-		  dvd=div;
-		  div=rem;
+	private long findLCM(long b, long c) {
+
+		long dvd = c;
+		long div = b;
+		while (dvd % div != 0) {
+			long rem = dvd % div;
+			dvd = div;
+			div = rem;
 		}
-		int gcd=div;
-		int lcm=(b*c)/gcd;
+		long gcd = div;
+		long lcm = (b * c) / gcd;
 		return lcm;
 	}
 
-
 	public static void main(String[] args) {
-		
-		int B=5;
-		int C=7;
-		int A=3;
-		
+
+		// Test1
+		// int B=5;
+		// int C=7;
+		// int A=3;
+
+		// Test2 with huge data
+		int A = 807414236;
+		int B = 3788;
+		int C = 38141;
+
 		FindAthMagicalNumber obj = new FindAthMagicalNumber();
-		
-		int result=obj.findAthMagicalNumber(A,B,C);
-		System.out.println(A+"th Magical number is "+result);
+		int result = obj.findAthMagicalNumber(A, B, C);
+		System.out.println(A + "th Magical number is " + result);
 	}
 }
