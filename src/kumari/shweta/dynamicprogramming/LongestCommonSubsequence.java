e0e188f2c8bf0167807b7fd9b985d22e41d5634a
shwetakiit <shweta0729e2open@gmail.com>
2024-10-11 12:04:17 +0530

    longest subsequence using dp

--- src/kumari/shweta/dynamicprogramming/LongestCommonSubsequence.java
+++ src/kumari/shweta/dynamicprogramming/LongestCommonSubsequence.java
@@ -8,9 +8,26 @@
    You need to return the length of such longest common subsequence.
    Input  A = "abbcdgf"  B = "bbadcgf"  Output Longest common string is bbcgf 
  */
-package kumari.shweta.sorting;
+package kumari.shweta.dynamicprogramming;
+
+import java.sql.SQLClientInfoException;
 
 public class LongestCommonSubsequence {
+	
+	
+	int lcsRecur(String s1, String s2, int i, int j) {
+
+		if (i < 0 || j < 0) {
+			return 0;
+		}
+
+		if (s1.charAt(i) == s2.charAt(j)) {
+			return 1 + lcsRecur(s1, s2, i - 1, j - 1);
+		} else {
+			return Math.max(lcsRecur(s1, s2, i - 1, j), lcsRecur(s1, s2, i, j - 1));
+		}
+
+	}
 	int LCS(String s1, String s2, int i, int j, int dp[][]) {
 
 		if (i <= 0 || j <= 0) {
@@ -27,6 +44,27 @@ public class LongestCommonSubsequence {
 		return dp[i][j];
 	}
 
+	
+	//With this approach no need to handle base case 
+	
+	int lcsWithoutBaseCase(String s1, String s2, int maxRowLength, int maxColLength, int dp[][]) {
+
+		// First row and first column of DP array is 0 for space
+
+		for (int i = 1; i <= maxRowLength; i++) {
+
+			for (int j = 1; j <= maxColLength; j++) {
+				if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+					dp[i][j] = 1 + dp[i - 1][j - 1];
+				} else {
+					dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+				}
+			}
+		}
+
+		return dp[maxRowLength][maxColLength];
+
+	}
 	public static void main(String[] args) {
 
 		String s1 = "abcd";
@@ -54,8 +92,19 @@ public class LongestCommonSubsequence {
 			System.out.println();
 		}
 		 
-		int longestSubString = obj.LCS(s1, s2, s1.length(), s2.length(), dp);
-		System.out.println("Longest sub string " + longestSubString);
+		//Using Dynamic programming 
+	   // int longestSubseq = obj.LCS(s1, s2, s1.length(), s2.length(), dp);
+		//System.out.println("Longest subsequence with 1st approach" + longestSubString);
+		
+		// Using recursion
+		int longestSub = obj.lcsRecur(s1, s2, s1.length() - 1, s2.length() - 1);
+
+		System.out.println("Longest sub sequence with second approach  " + longestSub);
+
+		int longestSubseq = obj.lcsWithoutBaseCase(s1, s2, s1.length(), s2.length(), dp);
+		System.out.println("Longest subsequence with 3rd approach " + longestSubseq);
 	}
+	
+
 
 }
