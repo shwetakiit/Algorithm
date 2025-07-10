f4fd7f68f11397153b741f9d8ccb41d2ff67b596
shwetakiit <shwetakumarikiit0729@gmail.com>
2025-07-10 10:27:35 +0530

    Find max value

--- src/kumari/shweta/array/FindMaxValue.java
+++ src/kumari/shweta/array/FindMaxValue.java
@@ -7,9 +7,11 @@ package kumari.shweta.array;
 import java.util.Arrays;
 
 /**
- * Given N array elements find Max value of (A[i]-A[j])+(i-j); input
+ * 1 )Given N array elements find Max value of (A[i]-A[j])+(i-j); input
  * arr[]={8,2,7,4,-2} --> Output -> 7
  * 
+ * 2)Given N array elements find max value of (A[i]-A[j])+(j-i)
+ *  
  */
 public class FindMaxValue {
 
@@ -24,11 +26,24 @@ public class FindMaxValue {
 		return max - min;
 	}
 
+
+	private static int findMaxValueOfGivenExpression(int[] arr) {
+
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = arr[i] - i;
+
+		}
+		int max = Arrays.stream(arr).max().getAsInt();
+		int min = Arrays.stream(arr).min().getAsInt();
+		return max - min;
+
+	}
 	public static void main(String[] args) {
 
 		int arr[] = { 8, 2, 7, 4, -2 };
 		int result = findMaxValue(arr);
 		System.out.println(result);
+		int value=findMaxValueOfGivenExpression(arr);
+		System.out.println("Max value of expression (A[i]-A[j])+(j-i) is "+value);
 	}
-
 }
