9b912089f244a7696494f81d921a1f4ced1f83bc
shwetakiit <shwetakumarikiit0729@gmail.com>
2025-07-11 23:17:08 +0530

    Find max value

--- src/kumari/shweta/array/FindMaxValue.java
+++ src/kumari/shweta/array/FindMaxValue.java
@@ -11,10 +11,19 @@ import java.util.Arrays;
  * arr[]={8,2,7,4,-2} --> Output -> 7
  * 
  * 2)Given N array elements find max value of (A[i]-A[j])+(j-i)
+ * 3)Given N elements find max value of |A[i]-A[j]|+|i-j|
+ *    here max {
+ *               A[i]-A[j]+i-j ,  ( A[i]+i) -(A[j]+j)//1 and 4 equations are same 
+ *               A[i]-A[J]+j-i      (A[i]-i)-(A[j]-j) // 2 and 3 equations are same 
+ *               A[j]-A[i]+i-j,    (A[j]-j)-(A[i]-i)
+ *                A[j]-A[i]+j-i
+ *               }
  *  
  */
 public class FindMaxValue {
 
+	// Question1
+	// TC->O(N) sc ->O(1)
 	public static int findMaxValue(int arr[]) {
 
 		for (int i = 0; i < arr.length; i++) {
@@ -26,6 +35,8 @@ public class FindMaxValue {
 		return max - min;
 	}
 
+	// Question2
+	// TC-> O(N) sc -> O(1)
 
 	private static int findMaxValueOfGivenExpression(int[] arr) {
 
@@ -38,12 +49,50 @@ public class FindMaxValue {
 		return max - min;
 
 	}
+
+	// Question 3
+	// TC->O(N) SC ->O(1)
+	public static int findMaxValueOfModExpression(int arr[]) {
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
+		int result = Integer.MIN_VALUE;
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] + i > max) {
+				max = arr[i] + i;
+			}
+			if (arr[i] + i < min) {
+				min = arr[i] + i;
+			}
+
+		}
+		result = Math.max(result, max - min);
+
+		for (int i = 0; i < arr.length; i++) {
+
+			if (arr[i] - i > max) {
+				max = arr[i] - i;
+			}
+			if (arr[i] - i < min) {
+				min = arr[i] - i;
+			}
+		}
+
+		result = Math.max(result, max - min);
+
+		return result;
+
+	}
+
 	public static void main(String[] args) {
 
 		int arr[] = { 8, 2, 7, 4, -2 };
 		int result = findMaxValue(arr);
 		System.out.println(result);
-		int value=findMaxValueOfGivenExpression(arr);
-		System.out.println("Max value of expression (A[i]-A[j])+(j-i) is "+value);
+		int value = findMaxValueOfGivenExpression(arr);
+		System.out.println("Max value of expression (A[i]-A[j])+(j-i) is " + value);
+		int expValue = findMaxValueOfModExpression(arr);
+		System.out.println("Max value of expression |A[i]-A[j]|+|i-j|" + expValue);
+
 	}
 }
